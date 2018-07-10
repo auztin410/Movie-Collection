5a65daeeea4c8ac83a73894b28f82c015ee9ca2d
auztin410 <auztin410@gmail.com>
2018-07-10 08:49:53 -0400

    Added add movie page and some rough logic

---
+++ addMovie.js
@@ -0,0 +1,14 @@
+var fs = require("fs");
+var addMovie = $("#addThisMovie");
+
+$("#addMovie").on("click", function (event) {
+event.preventDefault();
+fs.appendFile("movieCollection.txt", addMovie + ", ", function(err) {
+if(err) {
+    console.log(err);
+}
+else{
+    console.log("Movie Added Successfully")
+}
+});
+});
