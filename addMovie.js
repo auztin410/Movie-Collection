a30ab3e3a1ee7d9b293f2ebcee13c781bab3fe0c
auztin410 <auztin410@gmail.com>
2018-07-10 09:52:28 -0400

    Working on rating value.

--- addMovie.js
+++ addMovie.js
@@ -2,14 +2,14 @@ var fs = require("fs");
 var addMovie = $("#addThisMovie");
 
 $("#addMovie").on("click", function (event) {
-event.preventDefault();
-fs.appendFile("movieCollection.txt", addMovie + ", ", function(err) {
-if(err) {
-    console.log(err);
-}
-else{
-    console.log("Movie Added Successfully")
-}
-});
-$("#addMovieForm").reset();
+    event.preventDefault();
+    fs.appendFile("movieCollection.txt", addMovie + ", ", function (err) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log("Movie Added Successfully")
+        }
+    });
+    $("#addMovieForm").reset();
 });
